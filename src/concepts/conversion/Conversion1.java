b8445d6467c9cd3c13f75a736916c0a309f54816
Shubham Agrawal <shub8968@gmail.com>
2022-07-03 02:00:07 +0530

    Add Conversion1 Example

---
+++ src/concepts/conversion/Conversion1.java
@@ -0,0 +1,11 @@
+package concepts.conversion;
+
+public class Conversion1 {
+    
+    public static void main(String[] args) {
+        
+        char c = 's';
+        int ascii = (int)c;
+        System.out.println(ascii);
+    }
+}
